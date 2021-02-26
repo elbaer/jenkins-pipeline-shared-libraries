c39993d8fc98a9d0802ecaf2a4cd54df5dc0ba30
Christian Bär <baer@puzzle.ch>
2021-02-26 10:07:52 +0100

    test

--- src/com/puzzleitc/jenkins/command/CleanBuildsCommand.groovy
+++ src/com/puzzleitc/jenkins/command/CleanBuildsCommand.groovy
@@ -11,67 +11,41 @@ class CleanBuildsCommand {
     }
 
     void execute() {
-        def maxNumberToKeepBuilds = ctx.stepParams.getOptional('maxKeepBuilds', 10)
-
+        def maxNumberToKeepBuilds = ctx.stepParams.getOptional('maxKeepBuilds', 10) as Integer
         def environmentBuildCount = [:]
         def job = ctx.stepParams.getRequired("job")
-        try {
-            Jenkins.instance.getItemByFullName(job)
-                    .getBuilds()
-                    .findAll { it.isKeepLog() }
-                    .each { build ->
-                        def deployedEnvironment = []
-                        build.getActions(BadgeAction.class).each {
-                            deployedEnvironment << it.id
-                            environmentBuildCount[it.id] = environmentBuildCount.get(it.id, 0) + 1
-                        }
-
-                        // each Build that should be kept will be stored in keepBuild map
-                        def keepBuild = []
-                        deployedEnvironment.each {
-                            if (environmentBuildCount[it] <= maxNumberToKeepBuilds) {
-                                keepBuild << it
-                            }
-                        }
-
-                        // print out reason of/not keeping the build
-                        if (keepBuild) {
-                            ctx.info("Keeping build ${build} because of the following promotions: ${keepBuild.join(' ')}")
-                        } else {
-                            ctx.info("Deleting build ${build}")
-                            build.delete()
-                        }
-                    }
-        } catch (Exception e) {
-            ctx.fail("Exception: "+ e.getClass() + "\n" + e.getCause())
+        Jenkins.instance.getItemByFullName(job).getBuilds().findAll { it.isKeepLog() }.each { build ->
+                ctx.info("Keeping build ${build}")
         }
     }
-//    void execute() {
-//        def maxNumberToKeepBuilds = ctx.stepParams.getOptional('maxKeepBuilds', 10) as Integer
-//        def environmentBuildCount = [:]
-//        def job = ctx.stepParams.getRequired("job")
-//        Jenkins.instance.getItemByFullName(job).getBuilds().findAll { it.isKeepLog() }.each { build ->
-//            def deployedEnvironment = []
-//            build.getActions(BadgeAction.class).each {
-//                deployedEnvironment << it.id
-//                environmentBuildCount[it.id] = environmentBuildCount.get(it.id, 0) + 1
-//            }
+}
+
+//  void execute() {
+//   def maxNumberToKeepBuilds = ctx.stepParams.getOptional('maxKeepBuilds', 10) as Integer
+//   def environmentBuildCount = [:]
+//   def job = ctx.stepParams.getRequired("job")
+//   Jenkins.instance.getItemByFullName(job).getBuilds().findAll { it.isKeepLog() }.each { build ->
+//       def deployedEnvironment = []
+//       build.getActions(BadgeAction.class).each {
+//           deployedEnvironment << it.id
+//           environmentBuildCount[it.id] = environmentBuildCount.get(it.id, 0) + 1
+//       }
 //
-//            // each Build that should be kept will be stored in keepBuild map
-//            def keepBuild = []
-//            deployedEnvironment.each {
-//                if (environmentBuildCount[it] <= maxNumberToKeepBuilds) {
-//                    keepBuild << it
-//                }
-//            }
+//       // each Build that should be kept will be stored in keepBuild map
+//       def keepBuild = []
+//       deployedEnvironment.each {
+//           if (environmentBuildCount[it] <= maxNumberToKeepBuilds) {
+//               keepBuild << it
+//           }
+//       }
 //
-//            // print out reason of/not keeping the build
-//            if (keepBuild) {
-//                ctx.info("Keeping build ${build} because of the following promotions: ${keepBuild.join(' ')}")
-//            } else {
-//                ctx.info("Deleting build ${build}")
-//                build.delete()
-//            }
-//        }
-//    }
+//       // print out reason of/not keeping the build
+//       if (keepBuild) {
+//           ctx.info("Keeping build ${build} because of the following promotions: ${keepBuild.join(' ')}")
+//       } else {
+//           ctx.info("Deleting build ${build}")
+//           build.delete()
+//       }
+//   }
+//
 }
