9edb5db103cc880424727db2465defcb5b23c3cb
Christian Bär <baer@puzzle.ch>
2021-02-26 10:39:57 +0100

    test

--- src/com/puzzleitc/jenkins/command/CleanBuildsCommand.groovy
+++ src/com/puzzleitc/jenkins/command/CleanBuildsCommand.groovy
@@ -1,5 +1,6 @@
 package com.puzzleitc.jenkins.command
 
+import com.cloudbees.groovy.cps.NonCPS
 import com.jenkinsci.plugins.badge.action.BadgeAction
 import com.puzzleitc.jenkins.command.context.PipelineContext
 
@@ -13,11 +14,14 @@ class CleanBuildsCommand {
     void execute() {
         def job = ctx.stepParams.getOptional("job")
         try {
-            def getAllSuccessfulBuilds = Jenkins.instance.getItemByFullName(job)
-
-            ctx.info("Keeping build ${getAllSuccessfulBuilds}")
+            ctx.info("Keeping build: " + getSuccessfulBuilds(job))
         } catch (Exception e) {
             ctx.fail("Cannot find job: " + job)
         }
+    }
+
+    @NonCPS
+    Object getSuccessfulBuilds(String jobname) {
+        return Jenkins.instance.getItemByFullName(jobname)
     }
 }
