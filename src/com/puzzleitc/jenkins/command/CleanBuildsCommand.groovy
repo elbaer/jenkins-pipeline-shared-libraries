4b86ec8505658780bbb67691bdeb10ffa7969eb7
Christian Bär <baer@puzzle.ch>
2021-02-26 10:11:21 +0100

    test

--- src/com/puzzleitc/jenkins/command/CleanBuildsCommand.groovy
+++ src/com/puzzleitc/jenkins/command/CleanBuildsCommand.groovy
@@ -14,10 +14,9 @@ class CleanBuildsCommand {
         def maxNumberToKeepBuilds = ctx.stepParams.getOptional('maxKeepBuilds', 10) as Integer
         def environmentBuildCount = [:]
         def job = ctx.stepParams.getRequired("job")
-        Jenkins.instance.getItemByFullName(job).getBuilds().findAll { it.isKeepLog() }.each { build ->
+        def allJobBuilds = Jenkins.instance.getItemByFullName(job).getBuilds().findAll { it.isKeepLog() }
 
-            ctx.info("Keeping build ${build}")
-        }
+            ctx.info("Keeping build ${allJobBuilds}")
     }
 
 //  void execute() {
