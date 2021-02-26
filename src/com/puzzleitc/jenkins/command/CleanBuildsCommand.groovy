a5f847c7584abfd1cb23db2a53d9bfc24c69d288
Christian Bär <baer@puzzle.ch>
2021-02-26 10:03:57 +0100

    test

--- src/com/puzzleitc/jenkins/command/CleanBuildsCommand.groovy
+++ src/com/puzzleitc/jenkins/command/CleanBuildsCommand.groovy
@@ -15,32 +15,36 @@ class CleanBuildsCommand {
 
         def environmentBuildCount = [:]
         def job = ctx.stepParams.getRequired("job")
-        Jenkins.instance.getItemByFullName(job)
-                .getBuilds()
-                .findAll { it.isKeepLog() }
-                .each { build ->
-                    def deployedEnvironment = []
-                    build.getActions(BadgeAction.class).each {
-                        deployedEnvironment << it.id
-                        environmentBuildCount[it.id] = environmentBuildCount.get(it.id, 0) + 1
-                    }
+        try {
+            Jenkins.instance.getItemByFullName(job)
+                    .getBuilds()
+                    .findAll { it.isKeepLog() }
+                    .each { build ->
+                        def deployedEnvironment = []
+                        build.getActions(BadgeAction.class).each {
+                            deployedEnvironment << it.id
+                            environmentBuildCount[it.id] = environmentBuildCount.get(it.id, 0) + 1
+                        }
 
-                    // each Build that should be kept will be stored in keepBuild map
-                    def keepBuild = []
-                    deployedEnvironment.each {
-                        if (environmentBuildCount[it] <= maxNumberToKeepBuilds) {
-                            keepBuild << it
+                        // each Build that should be kept will be stored in keepBuild map
+                        def keepBuild = []
+                        deployedEnvironment.each {
+                            if (environmentBuildCount[it] <= maxNumberToKeepBuilds) {
+                                keepBuild << it
+                            }
+                        }
+
+                        // print out reason of/not keeping the build
+                        if (keepBuild) {
+                            ctx.info("Keeping build ${build} because of the following promotions: ${keepBuild.join(' ')}")
+                        } else {
+                            ctx.info("Deleting build ${build}")
+                            build.delete()
                         }
                     }
-
-                    // print out reason of/not keeping the build
-                    if (keepBuild) {
-                        ctx.info("Keeping build ${build} because of the following promotions: ${keepBuild.join(' ')}")
-                    } else {
-                        ctx.info("Deleting build ${build}")
-                        build.delete()
-                    }
-                }
+        } catch (Exception e) {
+            ctx.fail("Exception: " + e.printStackTrace())
+        }
     }
 //    void execute() {
 //        def maxNumberToKeepBuilds = ctx.stepParams.getOptional('maxKeepBuilds', 10) as Integer
